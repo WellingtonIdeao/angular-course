9059d7406433ea82527de4f0dd41a6c54307a532
Wellington Ideão <wellington.ideao@gmail.com>
2022-08-18 13:55:56 -0300

    My first component created by Angular CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyFirstComponent } from './my-first/my-first.component';
+import { MyFirst2Component } from './my-first2/my-first2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyFirstComponent,
+    MyFirst2Component,
   ],
   imports: [
     BrowserModule,
