d4ee77a4c4d1ef38c6fded084774e7bc5be74a32
Wellington Ideão <wellington.ideao@gmail.com>
2022-08-18 13:49:15 -0300

    My first component with Angular without to use Angular CLI

---
+++ src/app/my-first/my-first.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'my-first-component',
+    template: `
+    <p>My first component with Angular!</p>
+    `
+})
+export class MyFirstComponent{}
