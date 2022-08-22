ff47d6e5fbaa45056ed8408f15ccd743c1eb1db0
Wellington Ideão <wellington.ideao@gmail.com>
2022-08-22 17:36:53 -0300

    Diretiva ngStyle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,8 @@ import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
 import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
 import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.component';
+import { DiretivaNgstyleComponent } from './diretiva-ngstyle/diretiva-ngstyle.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -32,12 +34,14 @@ import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.co
     DiretivaNgswitchComponent,
     DiretivaNgforComponent,
     DiretivaNgclassComponent,
+    DiretivaNgstyleComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CursosModule,
     FormModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
